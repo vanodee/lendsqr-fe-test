0df93ec6dc125186f3dd87d95875aad927a063ae
vanodee <vanodee007@gmail.com>
2025-01-17 03:24:15 +0100

    chore: built LoginPage with Basic styling

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,50 @@
+import { useState } from 'react';
+import Logo from '../assets/lendsqr_logo.png';
+import LoginImg from '../assets/login_image.png';
 
 
 export default function LoginPage() {
+
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
   return (
-    <div>LoginPage</div>
+    <div className="loginPage-container">
+      <div className="loginPage-header">
+        <img src={Logo} alt="Lendsqr Logo" />
+      </div>
+
+      <img src={LoginImg} alt="Login Image" className="loginImage" />
+
+      <div className="loginForm-container">
+        <form id='loginForm'>
+          <div>
+            <h1 className='loginFont-xl-bold color-primary-100'>Welcome!</h1>
+            <h2 className='loginFont-md-normal color-accent-100'>Enter details to login.</h2> 
+          </div>
+          
+          <input
+            id="email"
+            name="email"
+            type="text"
+            placeholder="Email"
+          />
+
+          <input
+            id="password"
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+
+          <p className="loginFont-xs-semibold color-secondary-100">FORGOT PASSWORD ?</p>
+
+          <button className="loginFont-base-semibold" disabled={isLoading}>
+            {!isLoading ? "LOG IN" : "Please Wait..."}
+          </button>
+
+        </form>
+      </div>
+        
+    </div>
   )
 }
